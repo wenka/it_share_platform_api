706b56b12ed72fc0c7fdc13d841fcc6f0333a13d
wenka <wenkawk@outlook.com>
2017-05-16 09:30:35 +0800

    修改用户动态实体的注解

--- domain/src/main/java/com/wenka/domain/model/UserDynamic.java
+++ domain/src/main/java/com/wenka/domain/model/UserDynamic.java
@@ -1,11 +1,18 @@
 package com.wenka.domain.model;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.Date;
 
 /**
  * 用户动态
  * Created by 文卡<wkwenka@gmail.com> on 2017/5/15.
  */
+@Entity
+@Table(name = "user_dynamic")
 public class UserDynamic extends AbstractEntity {
 
     private User owner; //所属用户
@@ -14,6 +21,8 @@ public class UserDynamic extends AbstractEntity {
 
     private String msg; //消息
 
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "create_time", nullable = false)
     private Date createTime;
 
     private Integer state;
